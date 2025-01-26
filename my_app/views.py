e051ce1dbbfcab24cba094812c82bd0eb799d4b4
Ana Geguchadze <anageguchadze22@gmail.com>
2025-01-26 21:01:08 +0100

    Add Thank_you html

--- my_app/views.py
+++ my_app/views.py
@@ -14,3 +14,6 @@ def contact_view(request):
         message = request.POST.get('message')
         return redirect('thank_you')
     return render(request, 'contact.html')
+
+def thank_you_view(request):
+    return render(request, 'thank_you.html')
