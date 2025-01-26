b05c97cad3a0a9033c2ddbd3bb593fbcd2661edc
Ana Geguchadze <anageguchadze22@gmail.com>
2025-01-26 20:56:20 +0100

    Add views

--- my_app/views.py
+++ my_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def home_view(request):
+    return render(request, 'home.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        # ფორმის მონაცემები (შემდეგისთვის)
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        return redirect('thank_you')
+    return render(request, 'contact.html')
+
+def thank_you_view(request):
+    return render(request, 'thank_you.html')
